80f072c9b5fda9931d8c95a97b571dd397554d82
Anders Cedronius <anders.cedronius@netinsight.net>
2016-01-15 16:52:00 +0100

    Added Xcode project

---
+++ MyDearFriend/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyDearFriend
+//
+//  Created by Anders Cedronius on 15/01/16.
+//  Copyright © 2016 Anders Cedronius. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MyDearFriend/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MyDearFriend
+//
+//  Created by Anders Cedronius on 15/01/16.
+//  Copyright © 2016 Anders Cedronius. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
